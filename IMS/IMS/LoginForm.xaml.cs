6243f06e36b4a738dc208b62af0ffd788c51b042
gbuild <vesgostream@yandex.ru>
2017-04-13 18:35:57 +0500

    0.1

---
+++ IMS/IMS/LoginForm.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IMS
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginBox.Text.Length <1 || PasswordBox.Password.Length <1)
+            {
+                MessageBox.Show("Совет: Возможно вы не заполнили поля Логин|Пароль", "Ошибка авторизации пользователя");
+            }
+            else
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
